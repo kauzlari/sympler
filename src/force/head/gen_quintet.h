8d589a3349b1ed6dff54b3536092853ee6a56cbb
Daniel Robert Weilandt <daniel.r.weilandt@gmail.com>
2015-08-03 18:36:31 +0200

    Minor Corrections in Authors, change of Copyright Date

--- src/force/head/gen_quintet.h
+++ src/force/head/gen_quintet.h
@@ -2,7 +2,7 @@
  * This file is part of the SYMPLER package.
  * https://github.com/kauzlari/sympler
  *
- * Copyright 2002-2013, 
+ * Copyright 2002-2015, 
  * David Kauzlaric <david.kauzlaric@frias.uni-freiburg.de>,
  * and others authors stated in the AUTHORS file in the top-level 
  * source directory.
