e29c032067599aac2d2a7c93898088892c4b5de3
janmees <jmees001@gmail.com>
2016-12-21 12:32:33 +0100

    header included

--- src/boundary/head/boundary_stl_periodic.h
+++ src/boundary/head/boundary_stl_periodic.h
@@ -1,8 +1,31 @@
-/* 
- * File:   boundary_stl_periodic.h
- * Author: janmees
+/*
+ * This file is part of the SYMPLER package.
+ * https://github.com/kauzlari/sympler
  *
- * Created on December 5, 2016, 3:19 PM
+ * Copyright 2002-2016, 
+ * David Kauzlaric <david.kauzlaric@frias.uni-freiburg.de>,
+ * and others authors stated in the AUTHORS file in the top-level 
+ * source directory.
+ *
+ * SYMPLER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SYMPLER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SYMPLER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Please cite the research papers on SYMPLER in your own publications. 
+ * Check out the PUBLICATIONS file in the top-level source directory.
+ *
+ * You are very welcome to contribute extensions to the code. Please do 
+ * so by making a pull request on https://github.com/kauzlari/sympler
+ * 
  */
 
 #ifndef BOUNDARY_STL_PERIODIC_H
