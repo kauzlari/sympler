8216d9a0fe81457fea4700a20e0af1c2c89ff0e0
David Kauzlaric <david.kauzlaric@imtek.uni-freiburg.de>
2018-02-27 18:44:41 +0100

    Added abstract class PCacheIAPWSIF97

---
+++ src/basic/head/pca_iapws-if97.h
@@ -0,0 +1,295 @@
+/*
+ * This file is part of the SYMPLER package.
+ * https://github.com/kauzlari/sympler
+ *
+ * Copyright 2002-2018, 
+ * David Kauzlaric <david.kauzlaric@frias.uni-freiburg.de>,
+ * and others authors stated in the AUTHORS file in the top-level 
+ * source directory.
+ *
+ * SYMPLER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SYMPLER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SYMPLER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Please cite the research papers on SYMPLER in your own publications. 
+ * Check out the PUBLICATIONS file in the top-level source directory.
+ *
+ * You are very welcome to contribute extensions to the code. Please do 
+ * so by making a pull request on https://github.com/kauzlari/sympler
+ * 
+ */
+
+
+#ifndef __PCA_IAPWS_IF97_H
+#define __PCA_IAPWS_IF97_H
+
+#include "particle_cache.h"
+
+/*!
+ * General \a ParticleCache computing a thermodynamic variable or a 
+ * transport coefficient from two thermodynamic variables 'var1' and 
+ * 'var2' representing a thermodynamic state. Calculations are 
+ * performed based on IAPWS-IF97 (International Association for the 
+ * Properties of Water and Steam. Revised release on the IAPWS 
+ * industrial formulation 1997 for the thermodynamic properties of 
+ * water and steam. adadad, August 2007) using the freesteam-library 
+ * (http://freesteam.sourceforge.net/). Any instance of 
+ * \a PCacheIAPWSIF97 or of a derived class uses a lookup table (LUT) 
+ * in a predefined range 
+ * [m_var1Min, m_var2Min] x [m_var1Max, m_var2Max] 
+ * that is constructed once in advance to speedup the computation.
+ */
+class PCacheIAPWSIF97: public ParticleCache
+{
+
+ protected:
+
+  /*!
+   * The \a Symbol name of the first variable 'var1'
+   */
+  string m_var1Name;
+
+  /*!
+   * The \a Symbol name of the second variable 'var2'
+   */
+  string m_var2Name;
+
+  /*!
+   * \a Data tag offset of variable 'var1'
+   */
+  size_t m_var1Offset;
+
+  /*!
+   * \a Data tag offset of variable 'var2'
+   */
+  size_t m_var2Offset;
+
+  /*!
+   * Lower bound for variable 'var1' in the LUT 
+   */
+  double m_var1Min;
+
+  /*!
+   * Lower bound for variable 'var2' in the LUT 
+   */
+  double m_var2Min;
+
+  /*!
+   * Upper bound for variable 'var1' in the LUT 
+   */
+  double m_var1Max;
+
+  /*!
+   * Upper bound for variable 'var2' in the LUT 
+   */
+  double m_var2Max;
+
+  /*!
+   * Number of look-up values for variable 'var1' in the LUT.
+   */
+  int m_arraySizeVar1;
+
+  /*!
+   * Number of look-up values for variable 'var2' in the LUT.
+   */
+  int m_arraySizeVar2;
+
+  /*!
+   * Step size of look-up values for variable 'var2' internally derived 
+   * from \a m_var1Min, \a m_var1Max, and \a m_arraySizeVar1
+   */
+  double m_var1StepSize;
+
+  /*!
+   * Step size of look-up values for variable 'var2' internally derived 
+   * from \a m_var2Min, \a m_var2Max, and \a m_arraySizeVar2
+   */
+  double m_var2StepSize;
+
+  /*!
+   * Look-up table (LUT, 2D Array), where all look-up values are stored
+   */
+  double **m_LUT;
+
+  /*!
+  * Initialise the property list
+  */
+  virtual void init();
+
+  /*!
+   * Helper function for polymorphic copying
+   * Note the explicit copy of m_LUT
+   */
+  virtual ParticleCache* copyMySelf()
+  {
+    // The RHS should be called for an instantiatable subclass
+    ParticleCache* tmpPC = this->newInstance();
+
+    // points to same instance
+    PCacheIAPWSIF97* tmpPCIAPWS = (PCacheIAPWSIF97*) tmpPC;
+ 
+    tmpPCIAPWS->m_LUT = new double*[m_arraySizeVar2+1];
+    for (int i = 0; i <= m_arraySizeVar2; i++) {
+      tmpPCIAPWS->m_LUT[i] = new double [m_arraySizeVar1+1];
+    }
+    for(int j = 0; j < m_arraySizeVar1; j++) {
+      for (int i = 0; i < m_arraySizeVar2; i++) {
+	tmpPCIAPWS->m_LUT[j][i] = m_LUT[j][i];
+      }
+    }
+    
+    return tmpPC;
+  }
+
+  
+  /*!
+   * Adds the expressions used by this \a Symbol to the given list. 
+   * @param usedSymbols List to be filled with own instances of \a TypedValue
+   */
+  virtual void addMyUsedSymbolsTo(typed_value_list_t& usedSymbols)
+  {
+    
+  }
+  
+  /*!
+   * Returns the strings of those \a Symbols that the given class depends on
+   * due to hard-coded reasons (not due to runtime compiled expressions).
+   * @param usedSymbols List to add the strings to.
+   */
+  virtual void addMyHardCodedDependenciesTo(list<string>& usedSymbols) const
+  {
+    usedSymbols.push_back(m_var1Name);
+    usedSymbols.push_back(m_var2Name);
+  }
+
+  /*!
+   * General logic for determining the result by bilinear interpolation 
+   * based on the 4 support values of the LUT which surround 
+   * (\a inputVar1, \a inputVar2) 
+   * @param result Memory address for storing the result
+   * @param inputVar1 Value of thermodynamic input variable 'var1'
+   * @param inputVar2 Value of thermodynamic input variable 'var2'
+   */
+  void calculateResult(double& result, const double& inputVar1, const double& inputVar2) const;
+
+  
+  /*!
+   * In instances of subclasses, this function performs the actual
+   * call of the appropriate freesteam function  
+   * @param result Memory address for storing the result
+   * @param inputVar1 Value of thermodynamic input variable 'var1'
+   * @param inputVar2 Value of thermodynamic input variable 'var2'
+   */
+  virtual void freesteamCalculationForState(double& result, const double& inputVar1, const double& inputVar2) const = 0;
+  
+  /*!
+   * Instances of subclasses check if their individual constraints are 
+   * fulfilled
+   */
+  virtual void checkConstraints() = 0;
+
+  /*!
+   * Instances of subclasses return a new instance of their own class 
+   */
+  virtual ParticleCache* newInstance() = 0;
+  
+ public:
+  /*!
+   * Constructor
+   * @param colour The particle's color
+   * @param offset Tag offset of the local density
+   * @param wf The weighting function to use for the local density calculation
+   */
+  PCacheIAPWSIF97
+    (size_t colour, size_t offset, string symbolName);
+  
+  /*!
+   * Constructor
+   */
+  PCacheIAPWSIF97
+    (Simulation* parent);
+  
+  /*!
+   * Destructor
+   */
+  virtual ~PCacheIAPWSIF97(); 
+  
+  /*!
+   * Determines the result by bilinear interpolation based on the 4
+   * support values of the LUT which surround (\a var1, \a var2) 
+   * @param var1 Value of thermodynamic input variable 'var1'
+   * @param var2 Value of thermodynamic input variable 'var2'
+   */
+  /* virtual double calculateResult(const double& var1, const double& var2); */
+
+  /*!
+   * Precalculates the output value in the 'var1' and 'var2' ranges 
+   * specified by the corresponding private member variables
+   * The output values are stored in a Look-Up table (LUT, 2D Array) 
+   * with fixed step sizes given by \a m_stepSizeVar1 and 
+   * \a m_stepSizeVar2.
+   */
+  virtual void setupLUT();
+
+  /*!
+   * Calculates and stores the output for the given particle
+   * @param p The given particle
+   */
+  virtual void computeCacheFor(Particle* p) {
+    
+    Data& pTag = p->tag;
+    double& result = pTag.doubleByOffset(m_offset);
+    
+    calculateResult
+      (
+       result,
+       pTag.doubleByOffset(m_var1Offset),
+       pTag.doubleByOffset(m_var2Offset)
+       ); 
+  }
+
+  /*!
+   * Take steps necessary to register this calculator
+   */
+  virtual void registerWithParticle();
+
+  /*!
+   * Does this calculator equal \a c?
+   * @param c Other calculator
+   */
+  virtual bool operator==(const ParticleCache &c) const {
+    
+    if (typeid(c) == typeid(*this)) {
+      
+      return true;
+      /*m_wf->name() == cc->m_wf->name() && m_colour == cc->m_colour && m_stage == cc->m_stage && m_offset == cc->m_offset && m_symbolName == cc->m_symbolName;*/
+    } else {
+      return false;
+    }
+  } 
+  
+  /*!
+   * If it belongs to a Node structure, setup this instance of
+   * \a PCacheIAPWSIF97
+   */
+  virtual void setup();
+
+  /*!
+   * Returns the values stored in the LUT
+   */ 
+  virtual double** returnLUTvals() {
+    return m_LUT;
+  }
+
+};
+
+#endif
---
+++ src/basic/source/pca_iapws-if97.cpp
@@ -0,0 +1,265 @@
+/*
+ * This file is part of the SYMPLER package.
+ * https://github.com/kauzlari/sympler
+ *
+ * Copyright 2002-2018, 
+ * David Kauzlaric <david.kauzlaric@frias.uni-freiburg.de>,
+ * and others authors stated in the AUTHORS file in the top-level 
+ * source directory.
+ *
+ * SYMPLER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SYMPLER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SYMPLER.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Please cite the research papers on SYMPLER in your own publications. 
+ * Check out the PUBLICATIONS file in the top-level source directory.
+ *
+ * You are very welcome to contribute extensions to the code. Please do 
+ * so by making a pull request on https://github.com/kauzlari/sympler
+ * 
+ */
+
+
+#include "pca_iapws-if97.h"
+#include "simulation.h"
+#include "manager_cell.h"
+
+#define M_SIMULATION ((Simulation *) m_parent)
+#define M_PHASE M_SIMULATION->phase()
+#define M_MANAGER M_PHASE->manager()
+
+PCacheIAPWSIF97::PCacheIAPWSIF97
+  (size_t colour, size_t offset, string symbolName)
+  : ParticleCache(colour, offset, symbolName) {
+  m_stage = 0;
+  m_datatype = DataFormat::DOUBLE;
+}
+
+PCacheIAPWSIF97::PCacheIAPWSIF97
+    (/*Node*/Simulation* parent)
+  : ParticleCache(parent) {
+  m_stage = 0;
+  m_datatype = DataFormat::DOUBLE;
+  init();
+}
+
+
+PCacheIAPWSIF97::~PCacheIAPWSIF97()
+{
+  if(m_LUT) {
+    for (int i = 0; i < m_arraySizeVar2; ++i) {
+      if(m_LUT[i])
+	delete [] m_LUT[i];
+    }
+    delete [] m_LUT;
+  }
+}
+
+void PCacheIAPWSIF97::setupLUT() {
+
+  // auxiliary variables
+  double deltaVar1 = 0.;
+  double deltaVar2 = 0.;
+
+  // Temperature boarders for Region 3. See IAPWS-IF97 for more information.
+  checkConstraints();
+  // if (m_Tmin > 623.15 && m_Tmax < 863.15) {
+  
+  // Step sizes depending on the size of the Array and the ranges of the input values.
+  m_var1StepSize = (m_var1Max - m_var1Min)/(m_arraySizeVar1);
+  m_var2StepSize = (m_var2Max - m_var2Min)/(m_arraySizeVar2);
+  // Initialization of the 2D Array, depending on the given temperature and pressure sizes.
+  m_LUT = new double*[m_arraySizeVar2+1];
+  for (int i = 0; i <= m_arraySizeVar2; i++) {
+    m_LUT[i] = new double [m_arraySizeVar1+1];
+  }
+  // // Minimum density boarder along b23-line.
+  // double densityBoundary;
+  // Loop calculates the pressures in an adjusted temperature and density range and
+  // stores them into the LUT.
+  for(int j = 0; j < m_arraySizeVar1; j++) {
+    for (int i = 0; i < m_arraySizeVar2; i++) {
+      ////// START: PressureCalculation::localConstraintsMet(i, j)
+      // // Calculates the minimum and maximum density boarders to check
+      // // if the given density is in Region 3.	  
+      // double b23Pressure = freesteam_b23_p_T(deltaT + m_Tmin);
+      // SteamState S = freesteam_set_pT(b23Pressure, deltaT + m_Tmin);
+      // densityBoundary = freesteam_rho(S);
+      ////// END: PressureCalculation::localConstraintsMet(i, j)
+      
+      // if (localConstraintsMet(i, j)) {
+      // if (m_rhomin + rho > densityBoundary) {
+      // NOTE: include if (localConstraintsMet(i, j)) in freesteamCalculationForState(i,j)
+      
+      /* m_LUT[j][i] = freesteam_region3_p_rhoT(m_rhomin + rho, m_Tmin + deltaT)*/;
+      freesteamCalculationForState(m_LUT[j][i],
+				   m_var1Min + deltaVar1,
+				   m_var2Min + deltaVar2);
+      
+      deltaVar2 += m_var2StepSize;
+      
+      // } else {
+      // 	  throw gError("PCacheIAPWSIF97::setup", "Requested density and temperature parameters aren't within region 3. (See IAPWS-IF97 for more information): Unsuccessfully tried to compute P(T=" + ObjToString(deltaT+m_Tmin) + ",rho=" + ObjToString(m_rhomin+rho) + ") crossing rho_min(T) = " + ObjToString(densityBoundary));
+      //   }
+      
+    }
+    
+    deltaVar2 = 0.;
+    deltaVar1 += m_var1StepSize;
+    
+  }
+
+// NOTE: the following exception should be thrown in SubClass::checkConstraints()
+  // } else {
+  //    throw gError("PCacheIAPWSIF97::setup", "Requested temperature parameters aren't within region 3 (623.15K < T < 863.15K). (See IAPWS-IF97 for more information)");
+  // }     
+
+}
+
+void PCacheIAPWSIF97::calculateResult(double& result, const double& inputVar1, const double& inputVar2) const {
+
+  // out of bounds?
+  if((inputVar1 < m_var1Min) || (inputVar2 < m_var2Min)
+     || (inputVar1 > m_var1Max) || (inputVar2 > m_var2Max))    
+    throw gError("PCacheIAPWSIF97::calculateResult for module " + className(), "(" + m_var1Name + "," + m_var2Name + ") pair out of bounds: " + m_var1Name + "=" + ObjToString(inputVar1) + ", " + m_var2Name + "=" + ObjToString(inputVar2) + ", admissible [" + m_var1Name + "Min," + m_var1Name + "Max] = [" + ObjToString(m_var1Min) + "," + ObjToString(m_var1Max) + "], admissible [" + m_var2Name + "Min," + m_var2Name + "Max] = [" + ObjToString(m_var2Min) + "," + ObjToString(m_var2Max) + "].");
+
+  // Calculation of the surrounding sampling points 
+  int var1Point0 = (floor((inputVar1-m_var1Min)/m_var1StepSize));
+  int var2Point0 =(floor((inputVar2-m_var2Min)/m_var2StepSize));
+  int var1Point1 = (floor((inputVar1-m_var1Min)/m_var1StepSize)+1);
+  int var2Point1 =(floor((inputVar2-m_var2Min)/m_var2StepSize)+1);
+
+  // Normalisation of the input values.
+  double var1Normalised =
+    ((inputVar1 - (var1Point0*m_var1StepSize + m_var1Min))
+     / m_var1StepSize);
+  double var2Normalised =
+    ((inputVar2 - (var2Point0*m_var2StepSize + m_var2Min))
+     / m_var2StepSize);
+
+  // Bilinear interpolation of the normalized values.
+  result
+    = m_LUT[var1Point0][var2Point0]
+    *(1-var1Normalised)*(1-var2Normalised)
+    + m_LUT[var1Point1][var2Point0]
+    *var1Normalised*(1-var2Normalised)
+    + m_LUT[var1Point0][var2Point1]
+    *(1-var1Normalised)*var2Normalised
+    + m_LUT[var1Point1][var2Point1]
+    *var2Normalised*var1Normalised;
+}
+
+void PCacheIAPWSIF97::init()
+{
+  m_properties.setClassName("PCacheIAPWSIF97");
+  m_properties.setName("PCacheIAPWSIF97");
+  m_properties.setDescription
+    ("Computes a thermodynamic output variable or a transport "
+     "coefficient 'out' from two thermodynamic input variables 'var1' "
+     "and 'var2' representing a thermodynamic state (var1, var2). For "
+     "the specific "
+     "meaning of 'out', 'var1', 'var2', see the end of this "
+     "description (before the description of the attributes). "
+     "Calculations are performed based on IAPWS-IF97 (International "
+     "Association for the Properties of Water and Steam. Revised "
+     "release on the IAPWS industrial formulation 1997 for the "
+     "thermodynamic properties of water and steam. adadad, August "
+     "2007) using the freesteam-library "
+     "(http://freesteam.sourceforge.net/). Calculations are performed "
+     "based on a preconsdtructed lookup table (LUT) in a predefined "
+     "range [m_var1Min, m_var2Min] x [m_var1Max, m_var2Max] that is "
+     "constructed once in advance to speedup the computation."
+     ); 
+
+  STRINGPC
+      (symbol, m_symbolName,
+       "Symbol name for the computed output 'out'.");
+  STRINGPC
+      (var1, m_var1Name,
+       "Symbol name for 'var1'.");
+  STRINGPC
+      (var2, m_var2Name,
+       "Symbol name for 'var2'.");
+  DOUBLEPC
+      (var1Max, m_var1Max, 0,
+       "Upper limit for the admissible range of 'var1'.");
+  DOUBLEPC
+      (var1Min, m_var1Min, 0,
+       "Lower limit for the admissible range of 'var1'.");
+  DOUBLEPC
+      (var2Max, m_var2Max, 0,
+       "Upper limit for the admissible range of 'var2'.");
+  DOUBLEPC
+      (var2Min, m_var2Min, 0,
+       "Lower limit for the admissible range of 'var2'.");
+  INTPC
+      (LUTsizeVar1, m_arraySizeVar1, 0,
+       "The number of support values for 'var1' in the generated "
+       "look-up table.");
+  INTPC
+      (LUTsizeVar2, m_arraySizeVar2, 0,
+       "The number of support values for 'var2' in the generated "
+       "look-up table.");
+  
+  m_var1Name = "undefined";
+  m_var2Name = "undefined";
+  m_var1Min = HUGE_VAL;
+  m_var2Min = HUGE_VAL;
+  m_var1Max = -HUGE_VAL;
+  m_var2Max = -HUGE_VAL;
+  m_arraySizeVar1 = 0;
+  m_arraySizeVar2 = 0;
+}
+
+void PCacheIAPWSIF97::setup()
+{
+
+  ParticleCache::setup();
+
+  if(m_var1Name == "undefined")
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var1' has value \"undefined\""); 
+  if(m_var2Name == "undefined")
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var2' has value \"undefined\""); 
+
+  if(m_var1Min == HUGE_VAL)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var1Min' was not defined");
+  if(m_var2Min == HUGE_VAL)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var2Min' was not defined");
+  if(m_var1Max == -HUGE_VAL)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var1Max' was not defined");
+  if(m_var2Max == -HUGE_VAL)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var2Max' was not defined");
+  
+  if(m_arraySizeVar1 == 0)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'LUTsizeVar1' was not defined.");
+  if(m_arraySizeVar2 == 0)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'LUTsizeVar2' was not defined.");
+
+  if(m_var1Min >= m_var1Max)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var1Min' (= " + ObjToString(m_var1Min) + ") >= attribute 'var1Max' (= " + ObjToString(m_var1Max) + ") not meaningful!");
+  if(m_var2Min >= m_var2Max)
+    throw gError("PCacheIAPWSIF97::setup for module " + className(), "Attribute 'var2Min' (= " + ObjToString(m_var2Min) + ") >= attribute 'var1Max' (= " + ObjToString(m_var2Max) + ") not meaningful!");
+
+  // setup the look-up table
+  setupLUT();  
+
+  // Now, if m_species == "all", we can freely copy this PCA including
+  // m_LUT in function copyMySelf() called by registerCache()
+  registerCache();
+
+}
+
+
+void PCacheIAPWSIF97::registerWithParticle()
+{
+}
+
